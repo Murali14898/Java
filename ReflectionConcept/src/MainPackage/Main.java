a1c6bc1c5b9da23c972dcb44a1e5b7f4003c069d
Murali14898 <samalmurali575@gmail.com>
2026-03-14 18:37:47 +0530

    Reflection API concept

---
+++ ReflectionConcept/src/MainPackage/Main.java
@@ -0,0 +1,33 @@
+package MainPackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * Reflection in Java allows a program to inspect and manipulate classes, methods, 
+		 * fields, and constructors at runtime, even when their details are unknown at compile time. 
+		 * It is provided through the java.lang.reflect package and is widely used in frameworks and libraries.
+
+                To examine class metadata (methods, fields, constructors) during runtime
+                To create objects and invoke methods dynamically
+                To access and modify fields or methods whose names are determined at runtime
+                To build flexible, extensible frameworks that work with unknown classes
+                
+          *
+          *Java provides reflection through the following key classes:
+
+                Class
+                Constructor
+                Method
+                Field
+                
+           * ways to get it Class ?
+               1. using .class 
+               2. using obj.getClass()
+               3. using Class.forName("classname/fullfledged class name")
+		 */
+		
+
+	}
+
+}
---
+++ ReflectionConcept/src/module-info.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+/*
+		 * Reflection in Java allows a program to inspect and manipulate classes, methods, 
+		 * fields, and constructors at runtime, even when their details are unknown at compile time. 
+		 * It is provided through the java.lang.reflect package and is widely used in frameworks and libraries.
+
+                To examine class metadata (methods, fields, constructors) during runtime
+                To create objects and invoke methods dynamically
+                To access and modify fields or methods whose names are determined at runtime
+                To build flexible, extensible frameworks that work with unknown classes
+                
+          
+          *Java provides reflection through the following key classes:
+
+                Class
+                Constructor
+                Method
+                Field
+                
+           * ways to get it Class object ?
+               1. using .class 
+               2. using obj.getClass()
+               3. using Class.forName("classname/fullfledged class name")
+*/
+module ReflectionConcept {
+}
